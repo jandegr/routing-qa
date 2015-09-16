f9b6e2ea1350c1af68eed3f3624bf371bfe5915f
jandegr <jandegr@users.noreply.github.com>
2015-09-16 17:36:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -79,6 +79,7 @@ for filename in glob.glob('*.yaml'):
                  navit.set_center_by_string("geo: "+str(dataMap['capture']['lng']) + " " + str(dataMap['capture']['lat']))
             else:
                 navit.zoom_to_route()
+            time.sleep(0.5)    
             os.system("/usr/bin/import -window root "+gpx_directory+"/"+filename+export_suffix + ".png")
         else:
             print "No route found, last status : " + str(status) + ", duration : "+str(time.time() - start_time)
