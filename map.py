327b1b96544ef1021286e6ee96eda5577f32b654
jandegr <jandegr@users.noreply.github.com>
2019-06-19 13:11:02 +0200

    Update map.py

--- map.py
+++ map.py
@@ -5,8 +5,26 @@ import time
 import lxml.etree
 import sys
 import os
-import wget
+import requests
 
+
+def download(url, filename):
+    with open(filename, 'wb') as f:
+        response = requests.get(url, stream=True)
+        total = response.headers.get('content-length')
+
+        if total is None:
+            f.write(response.content)
+        else:
+            downloaded = 0
+            total = int(total)
+            for data in response.iter_content(chunk_size=max(int(total/1000), 1024*1024)):
+                downloaded += len(data)
+                f.write(data)
+                done = int(50*downloaded/total)
+                sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50-done)))
+                sys.stdout.flush()
+    sys.stdout.write('\n')
 
 
 
@@ -54,4 +72,4 @@ for filename in glob.glob('*.yaml'):
 url = "http://maps.navit-project.org/api/map/?bbox=" + str(mapleft) + "," + str(mapbottom) + "," + str(mapright) + "," + str(maptop) + ".bin"
 print ("url = ")
 print (url)
-wget.download(url, 'navit/bin/navit/maps/custommap.bin')  
+download(url, 'navit/bin/navit/maps/custommap.bin')  
